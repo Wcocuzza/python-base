f8f940ee9d9336e80fdddfa3efbba0d326edadec
wcocuzza <wallace.cocuzza@gmail.com>
2022-08-06 23:04:35 -0300

    Refatorado o 'read' e melhorado a questão de criar ou buscar notas

--- exercicios/notes.py
+++ exercicios/notes.py
@@ -30,31 +30,42 @@ if not arguments:
 if arguments[0] not in cmds:
     print(f"Invalid command: {arguments[0]}")
 
-if arguments[0] == "read":
-    for line in open(filepath):
-        title, tag, text = line.split("\t")
-        if tag.lower() == arguments[1].lower():
-            print(f"tile: {title}")
-            print(f"text: {text}")
-            print("_ * 30")
-            print()
-
-if arguments[0] == "new":
-    try:
-        title = arguments[1]
-        text = [
-        f"{title}\n",
-        input("tag: ").strip(),
-        input("text:\n").strip()
-    ]
-    except IndexError as e:
-        print(str(e))
+while True:
     
-    # \t - tsv
+    if arguments[0] == "new":
+        try:
+            title = arguments[1]
+        except IndexError:
+            title = input("Informe o titulo: ").strip().title()
+        
+        text = [
+            f"{title}",
+            input("tag: ").strip(),
+            input("text:\n").strip()
+        ]
+        # \t - tsv
 
-try:
-    with open(filepath, "a") as file_:
-        file_.write("\t".join(text) + "\n")
-except PermissionError as e:
-    print(str(e))
-    sys.exit(1)
+    if arguments[0] == "read":
+        try:
+            arg_tag = arguments[1].lower()
+        except IndexError:
+            arg_tag = input("Informe a tag: ").strip().lower()
+
+        for line in open(filepath):
+            title, tag, text = line.strip().split("\t")
+            if tag.lower() == arg_tag:
+                print(f"tile: {title}")
+                print(f"text: {text}")
+                print("_" * 30)
+                print()
+
+    try:
+        with open(filepath, "a") as file_:
+            file_.write("\t".join(text) + "\n")
+    except PermissionError as e:
+        print(str(e))
+        sys.exit(1)
+    
+    stop = input("Deseja continuar? [N/y]: ").strip().lower()
+    if stop != "y":
+        break
