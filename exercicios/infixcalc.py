114d9ac4d5c23651535b278e1d2ee6f697c49d39
wcocuzza <wallace.cocuzza@gmail.com>
2022-08-07 16:24:05 -0300

    Corrigido o erro de unpack e adicionado while para que o programa continue sua execução

--- exercicios/infixcalc.py
+++ exercicios/infixcalc.py
@@ -29,67 +29,73 @@ __version__ = "0.1.0"
 import os
 import sys
 from datetime import datetime
-arguments = sys.argv[1:]
 
-# TODO: Exceptions
-if not arguments:
-    operation = input("Operação: ")
-    n1 = input("n1: ")
-    n2 = input("n2: ")
-    arguments = [operation, n1, n2]
-elif len(arguments) != 3:
-    print("Número de argumentos inválidos!")
-    print("Ex: `sum 1 2`")
-    sys.exit(1)
+while True:
+    arguments = sys.argv[1:]
 
-operation, *nums = arguments
-
-valid_operations = ("sum", "sub", "mul", "div")
-if operation not in valid_operations:
-    print("Operação inválida")
-    print(valid_operations)
-    sys.exit(1)
-
-validated_nums = []
-
-for num in nums:
-    # TODO: Repeticao while + exceptions.\
-    if not num.replace(".", "").isdigit():
-        print(f"Número inválido {num}")
+    # TODO: Exceptions
+    if not arguments:
+        operation = input("Operação: ")
+        n1 = input("n1: ")
+        n2 = input("n2: ")
+        arguments = [operation, n1, n2]
+    elif len(arguments) != 3:
+        print("Número de argumentos inválidos!")
+        print("Ex: `sum 1 2`")
         sys.exit(1)
-    if "." in num:
-        num = float(num)
-    else:
-        num = int(num)
-    validated_nums.append(num)
 
-try:
-    n1, n2 = validated_nums
-except ValueError as e:
-    print(str(e))
-    sys.exit(1)
+    operation, *nums = arguments
 
-# TODO: Usar dict de funcoes
-if operation == "sum":
-    result = n1 + n2
-elif operation =="sub":
-    result = n1 - n2
-elif operation =="mul":
-    result = n1 * n2
-elif operation =="div":
-    result = n1 / n2
+    valid_operations = ("sum", "sub", "mul", "div")
+    if operation not in valid_operations:
+        print("Operação inválida")
+        print(valid_operations)
+        sys.exit(1)
 
-path = os.curdir
-filepath = os.path.join(path, "infixcalc.log")
-timestamp = datetime.now().isoformat()
-user = os.getenv("USER", "Anonymous")
+    validated_nums = []
 
-print(f"O Resultado é {result}")
+    for num in nums:
+        # TODO: Repeticao while + exceptions.\
+        if not num.replace(".", "").isdigit():
+            print(f"Número inválido {num}")
+            sys.exit(1)
+        if "." in num:
+            num = float(num)
+        else:
+            num = int(num)
+        validated_nums.append(num)
 
-try:
-    with open(filepath, "a") as file_:
-        file_.write(f"{timestamp} - {user} - {operation}, {n1}, {n2} = {result}\n")
-except PermissionError as e:
-    # TODO: logging
-    print(str(e))
-    sys.exit(1)
+    try:
+        n1, n2 = validated_nums
+    except ValueError as e:
+        print(str(e))
+        sys.exit(1)
+
+    # TODO: Usar dict de funcoes
+    if operation == "sum":
+        result = n1 + n2
+    elif operation =="sub":
+        result = n1 - n2
+    elif operation =="mul":
+        result = n1 * n2
+    elif operation =="div":
+        result = n1 / n2
+
+    path = os.curdir
+    filepath = os.path.join(path, "infixcalc.log")
+    timestamp = datetime.now().isoformat()
+    user = os.getenv("USER", "Anonymous")
+
+    print(f"O Resultado é {result}")
+
+    try:
+        with open(filepath, "a") as file_:
+            file_.write(f"{timestamp} - {user} - {operation}, {n1}, {n2} = {result}\n")
+    except PermissionError as e:
+        # TODO: logging
+        print(str(e))
+        sys.exit(1)
+    
+    cont = input("Deseja efetuar outra operação? [Y/n] ").strip().lower()
+    if cont != "y":
+        break
